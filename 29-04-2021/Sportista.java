fdcc2cfd6c8bdefe8cfe931b47f991acae206721
JelenaMiljic <84343355+JelenaMiljic@users.noreply.github.com>
2021-06-05 00:49:13 +0200

    Add files via upload

---
+++ 29-04-2021/Sportista.java
@@ -0,0 +1,44 @@
+package klase1;
+
+public class Sportista {
+
+	/*
+	 Napisati klasu Sportista. Sportista ima
+1) ime i prezime (polje tipa String) koje moze da se dohvati, ali ne i postavi
+2) sport kojim se bavi (polje tipa String) koje moze da se dohvati ali ne i postavi
+3) klub za koji igra (polje tipa String) koje moze da se dohvati. Sportista moze da promeni klub za koji igra.
+4) broj koji nosi na dresu ( tipa int) koje moze i da se dohvati i da se postavi. 
+
+	Napisati glavni program koji kreira tri igraca. U glavnom programu iskoristiti (proizvoljan broj puta) sve tipove metoda napisanih u klasi Sportista.
+	  */
+	private String imePrezime;
+	private String sport;
+	private String klub;
+	private int brojDresa;
+	
+	public Sportista (String imePrezime, String sport, String klub, int brojDresa) {
+		this.imePrezime=imePrezime;
+		this.sport=sport;
+		this.klub=klub;
+		this.brojDresa=brojDresa;
+	}
+	public String getImePrezime( ) {
+		return imePrezime;
+	}
+	
+	public String getSport() {
+		return sport;
+	}
+	public String getKlub() {
+		return klub;
+	}
+	public int getBrojDresa() {
+		return brojDresa;
+	}
+	
+	public void setbrojDresa(int brojDresa) {
+		this.brojDresa=brojDresa;
+	}
+	}
+
+
